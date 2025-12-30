b58e4ef737408b6ab1112649d3f28f1588173930
Alexandre Boutot <alexandreboutot@icloud.com>
2025-12-29 22:29:58 -0400

    Update highlights

--- lua/phosphor/init.lua
+++ lua/phosphor/init.lua
@@ -1,12 +1,20 @@
 local M = {}
 
 
+
 function M.colorscheme()
     vim.cmd.hi("clear")
     if vim.fn.has("syntax_on") then vim.cmd("syntax reset") end
 
     vim.o.background = "dark"
     vim.g.colors_name = "phosphor"
+
+    local palette = require("phosphor.palette")
+    local highlights = require("phosphor.highlights").get_groups(palette)
+
+    for h, set in pairs(highlights) do
+        vim.api.nvim_set_hl(0, h, set)
+    end
 
 end
 
